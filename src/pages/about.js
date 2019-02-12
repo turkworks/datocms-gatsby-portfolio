47d08cc0a9865a45542d8dcae6f3c7073e409467
turkworks <andrewcengiz@gmail.com>
2019-02-12 11:34:48 -0700

    image body

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,15 +9,18 @@ const About = ({ data: { about } }) => (
     <div className="sheet__inner">
       <h1 className="sheet__title">{about.title}</h1>
       <p className="sheet__lead">{about.subtitle}</p>
-      <div className="sheet__gallery">
-        <Img fluid={about.photo.fluid} />
+      <div className="sheet_content">
+        <div className="sheet__gallery">
+          <Img fluid={about.photo.fluid} />
+        </div>
+        <div
+          className="sheet__body"
+          dangerouslySetInnerHTML={{
+            __html: about.bioNode.childMarkdownRemark.html,
+          }}
+        />
       </div>
-      <div
-        className="sheet__body"
-        dangerouslySetInnerHTML={{
-          __html: about.bioNode.childMarkdownRemark.html,
-        }}
-      />
+      
     </div>
   </article>
 )
